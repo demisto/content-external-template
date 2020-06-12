e7b2a06f57024bf996e07d6171f554d020843b54
Anar Azadaliyev <aazadaliyev@paloaltonetworks.com>
2020-06-12 23:04:48 +0300

    clone helloworld via http

--- tools/update_helloworld_example.sh
+++ tools/update_helloworld_example.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 tmp_dir=$(mktemp -d)
-git clone git@github.com:demisto/content.git $tmp_dir
+git clone https://github.com/demisto/content.git $tmp_dir
 
 echo "Copying Hello World example from ${tmp_dir}/Packs/HelloWorld"
 rm -rf Packs/HelloWorld
