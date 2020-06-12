4974e8e9cb88f767abdfff7e183497f452be06e4
Anar Azadaliyev <aazadaliyev@paloaltonetworks.com>
2020-06-13 00:02:36 +0300

    create Packs dir

--- tools/update_helloworld_example.sh
+++ tools/update_helloworld_example.sh
@@ -5,6 +5,6 @@ GIT_LFS_SKIP_SMUDGE=1 git clone https://github.com/demisto/content.git $tmp_dir
 
 echo "Copying Hello World example from ${tmp_dir}/Packs/HelloWorld"
 rm -rf Packs/HelloWorld
-mv "${tmp_dir}/Packs/HelloWorld Packs"
+mv "${tmp_dir}/Packs/HelloWorld" Packs
 
 rm -rf ${tmp_dir}
