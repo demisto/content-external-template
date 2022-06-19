3ee65baab46cf7e170e25c97eaca91e5f22bd2ea
ilan <ierukhimovic@paloaltonetworks.com>
2022-06-19 11:13:57 +0300

    try

--- tools/update_repo.sh
+++ tools/update_repo.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
-
 output_paths=(
     "CommonServerPython.py"
     "CommonServerPowerShell.ps1"
@@ -23,19 +21,15 @@ url_to_fetch_from=(
     "https://raw.githubusercontent.com/demisto/content/master/Packs/Base/Scripts/CommonServerPowerShell/CommonServerPowerShell.ps1"
     "https://raw.githubusercontent.com/demisto/content/master/Tests/demistomock/demistomock.py"
     "https://raw.githubusercontent.com/demisto/content/master/Tests/demistomock/demistomock.ps1"
-    "https://raw.githubusercontent.com/demisto/content/master/Tests/scripts/dev_envs/default_python2/Pipfile"
-    "https://raw.githubusercontent.com/demisto/content/master/Tests/scripts/dev_envs/default_python2/Pipfile.lock"
-    "https://raw.githubusercontent.com/demisto/content/master/Tests/scripts/dev_envs/default_python3/Pipfile"
-    "https://raw.githubusercontent.com/demisto/content/master/Tests/scripts/dev_envs/default_python3/Pipfile.lock"
     "https://raw.githubusercontent.com/demisto/content/master/Tests/scripts/dev_envs/pytest/conftest.py"
-    "https://raw.githubusercontent.com/demisto/content/master/dev-requirements-py2.txt"
-    "https://raw.githubusercontent.com/demisto/content/master/dev-requirements-py3.txt"
+    "https://raw.githubusercontent.com/demisto/content/master/pyproject.toml"
+    "https://raw.githubusercontent.com/demisto/content/master/poetry.lock"
     "https://raw.githubusercontent.com/demisto/content/master/package.json"
     "https://raw.githubusercontent.com/demisto/content/master/package-lock.json"
+    "https://raw.githubusercontent.com/demisto/content/master/.hooks/pre-commit"
+    "https://raw.githubusercontent.com/demisto/content/master/.hooks/bootstrap
 )
 
-mkdir -p dev_envs/default_python2
-mkdir -p dev_envs/default_python3
 mkdir -p dev_envs/pytest
 
 is_failed=0
